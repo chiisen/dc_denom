dc32f82d7d7bb0f36fcee135d15a1a85b5a7e79f
chiisen <chiisen@hotmail.com>
2022-11-18 22:32:19 +0800

    fix: 修正單一HALL多幣別無法寫入問題

--- src/allDcDenom.js
+++ src/allDcDenom.js
@@ -18,6 +18,26 @@ function initAllDcDenom() {
     const denom_ = row_[3]
     const defaultDenomId_ = row_[4]
 
+    if (cid_ === undefined) {
+      console.warn("cid_ is undefined")
+    }
+
+    if (gameId_ === undefined) {
+      console.warn("gameId_ is undefined")
+    }
+
+    if (currency_ === undefined) {
+      console.warn("currency_ is undefined")
+    }
+
+    if (denom_ === undefined) {
+      console.warn("denom_ is undefined")
+    }
+
+    if (defaultDenomId_ === undefined) {
+      console.warn("defaultDenomId_ is undefined")
+    }
+
     const payLoad_ = {
       cid: cid_,
       gameId: gameId_,
@@ -27,34 +47,55 @@ function initAllDcDenom() {
     }
 
     if (gameId_ != "GameId") {
-      const valueGameIdCurrency_ = allDcDenomMap.get(cid_)
-      if (!valueGameIdCurrency_) {
-        const keyGameIdCurrency_ = "${gameId_}-${currency_}"
-        const newMapGameIdCurrency_ = new Map([[keyGameIdCurrency_, payLoad_]])
-        allDcDenomMap.set(cid_, newMapGameIdCurrency_)
+      const valueGameIdByCurrency_ = allDcDenomMap.get(cid_)
+      const newMapGameId_ = new Map()
+      newMapGameId_.set(gameId_, payLoad_)
+      const newMapCurrency_ = new Map()
+      newMapCurrency_.set(currency_, newMapGameId_)
+
+      if (!valueGameIdByCurrency_) {
+        //沒有 Cid
+        allDcDenomMap.set(cid_, newMapCurrency_)
       } else {
-        valueGameIdCurrency_.set(gameId_, payLoad_)
-        allDcDenomMap.set(cid_, valueGameIdCurrency_)
+        //有 Cid
+        const valueCurrency_ = valueGameIdByCurrency_.get(currency_)
+        if (!valueCurrency_) {
+          //沒有 Currency
+          valueGameIdByCurrency_.set(currency_, newMapGameId_)
+        } else {
+          //有 Currency，檢查 GameId
+          const valueGameId_ = valueCurrency_.get(gameId_)
+          if (!valueGameId_) {
+            //沒有 GameId
+            valueCurrency_.set(gameId_, payLoad_)
+          } else {
+            //有 GameId
+            console.error(`重複的 Currency: ${currency_} GameId: ${gameId_}`)
+          }
+        }
       }
     }
   })
 }
 
 /**
- *
+ * 輸出所有 DC 的 denom 到 EXCEL
  */
 function exportAllDcDenomToExcel() {
-  allDcDenomMap.forEach((valueGameIdCurrency_, keyCid_) => {
+  allDcDenomMap.forEach((valueCurrency_, keyCid_) => {
     let buff = []
 
-    let sheetName = hallNameMap.get(keyCid_)
-    if (!sheetName) {
+    const hallName_ = hallNameMap.get(keyCid_)
+    if (!hallName_) {
       console.error(`找不到 Cid: ${keyCid_}`) //@note 這是異常，先以 HALL_LIST 為主
+      return //沒有 hallName 後面就不處理了
     } else {
-      let excelFishData = []
+      let excelData = []
+
+      let sheetName_
 
       //標題
-      excelFishData.push([
+      excelData.push([
         "Cid",
         "GameId",
         "Currency",
@@ -92,7 +133,8 @@ function exportAllDcDenomToExcel() {
         "預設面額",
       ])
 
-      excelFishData.push([
+      //面額標題
+      excelData.push([
         "",
         "",
         "",
@@ -130,36 +172,45 @@ function exportAllDcDenomToExcel() {
         "",
       ])
 
-      valueGameIdCurrency_.forEach((value_) => {
-        let defaultDenomString_ = ""
-        if (value_.defaultDenomId != 0) {
-          defaultDenomString_ = denomIndexToDenomString(value_.defaultDenomId)
-        } else {
-          defaultDenomString_ = denomIndexListStringToDefaultDenomString(value_.denom)
-        }
+      valueCurrency_.forEach((valueGameId_, keyCurrency_) => {
+        sheetName_ = keyCurrency_
 
-        console.log(value_.denom)
-        const excelDenomList_ = convertDenomListStringToExcelDenomList(value_.denom)
+        valueGameId_.forEach((valuePayLoad_, keyGameId_) => {
+          let defaultDenomString_ = ""
+          if (valuePayLoad_.defaultDenomId != 0) {
+            if (valuePayLoad_.defaultDenomId === undefined) {
+              const msg_ = `[Cid: ${keyCid_} Currency: ${keyCurrency_} GameId: ${keyGameId_}] valuePayLoad_.defaultDenomId is undefined`
+              console.error(msg_)
+              return // 資料有異常，下面不處理了
+            } else {
+              defaultDenomString_ = denomIndexToDenomString(valuePayLoad_.defaultDenomId)
+            }
+          } else {
+            defaultDenomString_ = denomIndexListStringToDefaultDenomString(valuePayLoad_.denom)
+          }
 
-        const denomListString_ = denomIndexListStringToDenomListString(value_.denom)
+          const excelDenomList_ = convertDenomListStringToExcelDenomList(valuePayLoad_.denom)
 
-        excelFishData.push([
-          value_.cid,
-          value_.gameId,
-          value_.currency,
-          denomListString_,
-          ...excelDenomList_,
-          value_.defaultDenomId,
-          defaultDenomString_,
-        ])
-      })
+          const denomListString_ = denomIndexListStringToDenomListString(valuePayLoad_.denom)
 
-      const oneSheetData = { name: `${sheetName}`, data: [...excelFishData] }
-      buff.push(oneSheetData)
+          //寫入一筆資料的EXCEL
+          excelData.push([
+            valuePayLoad_.cid,
+            valuePayLoad_.gameId,
+            valuePayLoad_.currency,
+            denomListString_,
+            ...excelDenomList_,
+            valuePayLoad_.defaultDenomId,
+            defaultDenomString_,
+          ])
+        }) // valueGameId_ end
+        const oneSheetData = { name: `${sheetName_}`, data: [...excelData] }
+        buff.push(oneSheetData)
+      }) // valueCurrency_ end
+    } //else end
 
-      writeMultiplePagesExcel(`./output/${sheetName}.xlsx`, buff)
-    }
-  })
+    writeMultiplePagesExcel(`./output/${hallName_}.xlsx`, buff)
+  }) // allDcDenomMap end
 }
 
 module.exports = {
